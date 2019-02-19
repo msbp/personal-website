df6fef9d55d396e99113b66aca4e596eb3ba0a75
msbp <mauricio.browne@gmail.com>
2019-02-19 10:27:03 -0800

    Moved content.json file. Also added photo to home page.

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import '../css/Sections.css'
+const homeContent = require('../config/content.json').sections.home
 
 class Home extends React.Component {
-    
+
     /**
      * This is the component's constructor.
      * @param {*} props - Props
      */
     constructor(props) {
         super(props)
+    }
+
+    getPhoto() {
+        return (
+            <span>
+                <img src={homeContent.profileImage} />
+            </span>
+        )
+
     }
 
     /**
@@ -18,6 +28,7 @@ class Home extends React.Component {
         return (
             <div className="home">
                 <h1>This is the home content.</h1>
+                {this.getPhoto()}
             </div>
         )
     }
